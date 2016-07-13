c31505b1494045d3f44831d53195ae1ad578446b
fabry <fabry_00@hotmail.com>
2016-07-13 21:13:07 +0200

    removed git from webapp

--- webapp/src/app/task/task.component.ts
+++ webapp/src/app/task/task.component.ts
@@ -10,16 +10,18 @@ import { Task } from './task'
 })
 export class TaskComponent implements OnInit {
 
-  task : Task;
+  tasks : Task[];
 
   constructor(private _logger:Logger){
     this._logger.debug('TaskComponent constructor');
   }
 
   ngOnInit() {
-    this.task = new Task();
-    this.task.name = "Test";
-    this.task.id = 2;
+    this._logger.debug('ngOnInit');
+    this.tasks = new Array(1);
+    this.tasks[0] = new Task();
+    this.tasks[0].name = "Test";
+    this.tasks[0].id = 2;
   }
 
 }
