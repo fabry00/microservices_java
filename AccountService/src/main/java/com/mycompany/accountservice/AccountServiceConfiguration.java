8f0aed78003dc97bd8281cda29969dd74db4b313
fabry <fabry_00@hotmail.com>
2016-07-15 16:14:11 +0200

    added configuration

--- AccountService/src/main/java/com/mycompany/accountservice/AccountServiceConfiguration.java
+++ AccountService/src/main/java/com/mycompany/accountservice/AccountServiceConfiguration.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
 import javax.validation.constraints.*;
 
+/**
+ * Config example https://github.com/dropwizard/dropwizard/tree/master/dropwizard-example
+ * @author fabry
+ */
 public class AccountServiceConfiguration extends Configuration {
     // TODO: implement service configuration
 }
