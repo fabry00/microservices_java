2cf98dac2b859673b8b860e6d6ea98194858c9a4
fabry <fabry_00@hotmail.com>
2016-07-21 18:46:47 +0200

    added reporting

--- update_compile.sh
+++ update_compile.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]
     echo "No argument supplied. ARG0=y to site or n only to pull and compile"
 	exit
   else
-    SITE="&& mvn site"
+    SITE=$1
 fi
 
 echo "-------------------------------------"
@@ -19,22 +19,46 @@ echo "-------------------------------------"
 echo "-------------------------------------"
 echo "COMPILING COMMONS"
 cd Commons 
-mvn package && mvn install $SITE
+mvn package && mvn install
+if [ $SITE == "y" ]
+    then
+	echo "-------------------------------------"
+	echo "SITE COMMONS"
+	  mvn site
+fi
 
 echo "-------------------------------------"
 echo "-------------------------------------"
 echo "COMPILING ProcessService"
-cd ProcessService 
-mvn package && mvn install $SITE
+cd ../ProcessService 
+mvn package && mvn install
+if [ $SITE == "y" ]
+    then
+	echo "-------------------------------------"
+	echo "SITE ProcessService"
+	  mvn site
+fi
 
 echo "-------------------------------------"
 echo "-------------------------------------"
 echo "COMPILING AccountService"
-cd AccountService 
-mvn package && mvn install $SITE
+cd ../AccountService 
+mvn package && mvn install
+if [ $SITE == "y" ]
+    then
+	echo "-------------------------------------"
+	echo "SITE AccountService"
+	  mvn site
+fi
 
 echo "-------------------------------------"
 echo "-------------------------------------"
 echo "COMPILING ServiceAggregator"
-cd ServiceAggregator 
-mvn package && mvn install $SITE
+cd ../ServiceAggregator 
+mvn package && mvn install
+if [ $SITE == "y" ]
+    then
+	echo "-------------------------------------"
+	echo "SITE ServiceAggregator"
+	  mvn site
+fi
