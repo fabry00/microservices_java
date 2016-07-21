f7a9201b6000aff5806c75181e000bc5cba66413
fabry <fabry_00@hotmail.com>
2016-07-21 18:41:36 +0200

    added reporting

--- update_compile.sh
+++ update_compile.sh
@@ -1,22 +1,40 @@
 #!/bin/sh
 
-git pull
 SITE=""
 if [ -z "$1" ]
   then
     echo "No argument supplied. ARG0=y to site or n only to pull and compile"
+	exit
   else
     SITE="&& mvn site"
 fi
 
+echo "-------------------------------------"
+echo " UPDATEING FORM REPOSITORY"
+git pull
+
+echo "-------------------------------------"
+echo "-------------------------------------"
+
+echo "-------------------------------------"
+echo "COMPILING COMMONS"
 cd Commons 
 mvn package && mvn install $SITE
 
+echo "-------------------------------------"
+echo "-------------------------------------"
+echo "COMPILING ProcessService"
 cd ProcessService 
 mvn package && mvn install $SITE
 
+echo "-------------------------------------"
+echo "-------------------------------------"
+echo "COMPILING AccountService"
 cd AccountService 
 mvn package && mvn install $SITE
 
+echo "-------------------------------------"
+echo "-------------------------------------"
+echo "COMPILING ServiceAggregator"
 cd ServiceAggregator 
 mvn package && mvn install $SITE
